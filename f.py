c9dc69575da846b76e50ce6767db424dd5f26aa7
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-02 09:44:04 -0700

    Update f.py

--- f.py
+++ f.py
@@ -1,11 +1,8 @@
 r=int(input())
-r==2
-print("yes")
 for i in range(2,r):
     d=r%i
     if d==0:
         print("no")
         break
-    else:
-        print("yes")
-        break
+else:
+    print("yes")
